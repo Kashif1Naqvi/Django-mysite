eb9ff4b97acaf502f43ed40103bdc7033a783a2e
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-11 16:14:56 +0500

    fix error

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 # import model 
 from .models import Question
 # Create your views here.
@@ -13,14 +14,14 @@ def index(request):
     }
     return render(request,'polls/index.html',context)
 
-# def detail(request,question_id):
-#     return HttpResponse("You're looking at question %s." %question_id)
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s." %question_id)
 
 
-# def results(request,question_id):
-#     response = "You're looking at the results of question %s"
-#     return HttpResponse(response % question_id)
+def results(request,question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
 
-# def vote(request,question_id):
-#     return HttpResponse("You're voting on question %s."%question_id)
+def vote(request,question_id):
+    return HttpResponse("You're voting on question %s."%question_id)
 
