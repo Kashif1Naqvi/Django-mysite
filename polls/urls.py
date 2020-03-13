ec53e92ad582147a9d0b037f213465f3cee5cf40
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-13 15:51:57 +0500

    results route error fixed

--- polls/urls.py
+++ polls/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #/polls/5(or any id)
     path('<int:pk>/', views.DetailView.as_view() , name='detail' ),
     # /polls/5/result/
-    path('<int:pk>/results',views.ResultsView.as_view(), name="results"),
+    path('<int:pk>/results/',views.ResultsView.as_view(), name="results"),
     # /polls/5/vote/
     path('<int:question_id>/vote/', views.vote , name="vote" )
 ]
--- polls/views.py
+++ polls/views.py
@@ -19,7 +19,7 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/details.html'
+    template_name = 'polls/detail.html'
 
 class ResultsView(generic.DetailView):
     model = Question
