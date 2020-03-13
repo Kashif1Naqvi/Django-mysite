7815889126c57b594a83d1b478e507d0289091ed
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-13 18:19:28 +0500

    get_queryset added in views

--- polls/views.py
+++ polls/views.py
@@ -17,11 +17,14 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """ Return the five published questions (not including those set to be published in the future) """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
+    def get_queryset(self):
+        """
+            Excludes any question that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
