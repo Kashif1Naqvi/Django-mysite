6977896c3d7f55f37a8ed9c9adf0c2f4bc7e4d22
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-13 19:05:14 +0500

    test new class added

--- polls/tests.py
+++ polls/tests.py
@@ -85,3 +85,15 @@ class QuestionModelTests(TestCase):
             self.assertQuerysetEqual(
               responce.context['latest_question_list'] , ['<Question:Past question 1.>']
             )
+    class QuestionDetailViewTests(TestCase):
+        def test_future_question(self):
+            """
+            The detail view of a question with a pub_date in the future
+            returns a 404 not found.
+            """
+            future_question = create_question(question_text='Future question', days=5)
+            url = reverse('polls:index', args=(future_question.id,))
+            response = self.client.get(url)
+            self.assertEqual(response.status_code,404)
+        
+        
