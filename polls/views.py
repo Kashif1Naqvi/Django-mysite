8feeab7d23a821fe1d5460f3b56499732f485cb4
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-13 17:28:09 +0500

    filter data views get_queryset method

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect,HttpResponse
 # import model 
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Question,Choice
 # Create your views here.
 
@@ -14,8 +15,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """ Return the five published questions """
-        return Question.objects.order_by('-pub_date')[:5]
+        """ Return the five published questions (not including those set to be published in the future) """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
