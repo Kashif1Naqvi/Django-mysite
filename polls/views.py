7942fdfa926fffb96c5d1bdc831b4f542a79f8e6
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-13 15:20:52 +0500

    index funciton changed in class

--- polls/views.py
+++ polls/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponseRedirect,HttpResponse
 # import model 
 from django.urls import reverse
+from django.views import generic
 from .models import Question,Choice
 # Create your views here.
 
 # when everything is done then not need to load loder and httpResponce we do every thing with render
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list':latest_question_list,
-    }
-    return render(request,'polls/index.html',context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ Return the five published questions """
+        return Question.objects.order_by('-pub_date')[:5]
+
 
 def detail(request,question_id):
     
